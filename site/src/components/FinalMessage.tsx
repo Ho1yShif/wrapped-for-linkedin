f20e048476a607d971d0ccebb6a7e56d6209f563
Ho1yShif <shifraisaacs@gmail.com>
2025-12-12 11:45:25 -0800

    style: final message styles

--- site/src/components/FinalMessage.tsx
+++ site/src/components/FinalMessage.tsx
@@ -5,7 +5,7 @@ export const FinalMessage: React.FC = () => {
   return (
     <div className="final-message">
       <h1 className="final-message-title">That's a wrap!</h1>
-      <p className="final-message-subtitle">See you next year 👋</p>
+      <p className="final-message-subtitle">See you next year <img src="/images/dashboard/hello.png" alt="wave" className="wave-image" /></p>
     </div>
   );
 };
